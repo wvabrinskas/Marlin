a695767deade0d948ea554f3e603255d791cd7e4
William Vabrinskas <wvabrinskas@gmail.com>
2020-10-26 09:14:57 -0400

    update profiles

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1462,7 +1462,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 200
+#define PREHEAT_1_TEMP_HOTEND 205
 #define PREHEAT_1_TEMP_BED     55
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
